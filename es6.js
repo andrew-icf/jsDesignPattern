a42ada37a526fb9b33b49c531137904f52b7aa56
Andrew Vandermeer <innerchaosfitness@yahoo.com>
2018-02-19 11:10:40 -0700

    destructor, for of, and template literal

--- es6.js
+++ es6.js
@@ -26,7 +26,7 @@ var getProduct = function(productId = 10, type = 'software') {
 getProduct(undefined, 'hardware'); // returns 10 hardware, the default 10 will still be used if there is one
 
 // Rest and Spread
-//       REST OPERATOR
+//                  REST OPERATOR
 var showCategories = function(productId, ...categories) {
   console.log(categories instanceof Array); // returns true
   console.log(categories); // returns ['search', 'advertising']
@@ -37,7 +37,7 @@ var showCategories = function(productId, ...categories) {
 };
 showCategories(123); // returns []
 
-//       SPREAD OPERATOR
+//                  SPREAD OPERATOR
 var prices = [12, 20 8];
 var maxPrice = Math.max(...prices);
 console.log(maxPrice); // returns 20, so the spread operator took the array and divided it into three separate values
@@ -46,7 +46,7 @@ console.log(maxCode); // returns 4, spread operator will also work on strings
 var codeArray = ['a', ...'bcd', 'e'];
 console.log(codeArray); // does not make a multi-dimensional array will return ['a', 'b', 'c', 'd', 'e'];
 
-// OBJECT LITERAL EXTENSIONS
+//                  OBJECT LITERAL EXTENSIONS
 var field = 'dynamicField';
 var price = 5.99;
 var ident = 'productId';
@@ -61,3 +61,64 @@ var productView = {
   get [ident] () {return true;}
   set [ident] (value) {}       // these work for getters and setters as well
 };
+
+//                For of Loop => works with iterables
+var categories = ['a', 'b', 'c'];
+for(var item of categories) {
+  console.log(item); // returns a, b, c
+}
+// essentially this 'for of' loop looped through each element of categories, you can also iterate the characters in a string
+
+//                  Octals and Binary literals in es6
+var value = 0o10;
+console.log(value); // returns 8, this is how we specify an octal value. Beginning with the number 0 and then the letter o(case insensitive)
+
+var value = 0b10;
+console.log(value); // returns 2, specifying a Binary value with the number 0 and the letter b
+
+//                  Template Literals
+// string template with interpolation on literals and expressions
+let invoiceNum = '1450';
+console.log(`Invoice number: ${invoiceNum}`);
+console.log(`Invoice number: ${'Inv-' + invoiceNum}`); // returns Invoice number: Inv-1450 (expression)
+// If calling this in a function the interpolation will take place first so be careful as to the scope of the variable that you think you're setting
+
+// a tagged template literal
+function processInvoice(segments){
+  console.log(segments);
+}
+processInvoice `template`; // returns an array ["template"] this is how we can create a function to handle a template
+
+//                  DESTRUCTORING (taking apart the structure of something)
+
+// Ex: removing the individual indexes or elements in an array or an object where we can destructor or take out individual fields and re assign them
+let salary = [20, 45, 1345];
+let [low, average, high ] = salary;
+console.log(average); // returns 45. The elements in the array were assigned to the variables
+
+let salary = [20, 45];
+let [low, average, high ] = salary;
+console.log(high); // returns undefined, no third element you can skip elements by adding an extra comma[low, , high]
+let [low, ...remaining]; // returns whatever is after the first element in an array
+// can use a default as well [low, average, high = 8000];
+// nested values in a multi-dimensional Array
+let [low, average, [actualLow, actaulHigh]];
+// as a function
+function reviewSalary([low, average], high = '80') {
+  console.log(average);
+}
+reviewSalary(['4', '6']); // returns 6
+
+//  FOR AN OBJECT
+let salary = {
+  low: '34',
+  average: '87',
+  high: '999'
+}
+let {low, average, high} = salary;
+console.log(high); // returns 999, properties need to match up between the object and our new variables
+let { low: newLow } = salary; // we can re-asign the property to a new name by doing it like this
+
+for(let [a, b] of [[5, 10]]) {
+  console.log(`${a} ${b}`); // returns 5 10, destructoring works in a for of loop as well
+}
